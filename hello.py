4eb14b6057bd1ef437b535399546077e1d0e8cab
Rafael Dantas <raffaeldantas@gmail.com>
2023-08-26 23:03:47 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,8 +1,22 @@
 import streamlit as st
-import mymodel as m
+import geopandas as gpd
+import matplotlib.pyplot as plt
 
-st.write("""
-# Sales Model
-Below are or sales predictions
-for this customer.
-""")
+# Carrega o shapefile dos estados do Brasil
+brasil = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+estados = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+
+# Filtra os estados do Brasil
+brasil = brasil[brasil['name'] == 'Brazil']
+
+# Carrega o shapefile dos estados do Brasil
+estados = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+
+# Plota o mapa do Brasil com os estados
+fig, ax = plt.subplots(figsize=(10, 8))
+brasil.plot(ax=ax, color='lightgray', edgecolor='black')
+estados.boundary.plot(ax=ax, linewidth=1, color='black')
+
+ax.set_title('Mapa do Brasil com Estados')
+plt.axis('off')  # Desativa os eixos
+plt.show()
